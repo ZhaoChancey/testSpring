07aecb74523b921f7c04cba703f3707b66b9f5b6
ZhaoChancey <61592919+ZhaoChancey@users.noreply.github.com>
2021-07-03 16:58:49 +0800

    Update testGit.java

--- src/testGit.java
+++ src/testGit.java
@@ -7,6 +7,6 @@ public class testGit {
     public static void main(String[] args) {
         System.out.println("新建");
         System.out.println("更新");
-        System.out.println("remote1");
+        System.out.println("remote");
     }
 }
